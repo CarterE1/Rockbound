c97b416aa7b3c746424a600cecbf374f56efc299
Carter Ellis <cjellis2@icloud.com>
2024-10-06 21:17:02 -0400

    Major movement changes
    - Cant walk while in air
    - Added ability to simply click mouse to throw rocks
    - Fixed charged rock throw + 'teleporting' when throwing
    - Added small debounce to throwing
    - Added extremely simple ui (for rock count and debug)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Vector2 throwVector;
 
+    private Vector2 throwVector;
     private bool throwing; // is the player throwing
     private float throwForce; // how long player is holding left click
 
+    private float throwDebounce; // debounce vars
+    private bool canThrow;
+
     [Header("Movement Props")]
-    public float speed;
+    public float horizontalSpeed;
     public float throwForceMultiplier;
-    public float maxVelocity;
+    public float maxThrowForce;
+    public float initalForce; // How much force is applied if the player simply clicks
+    public float throwCooldown;
+    public int rockCount; // Does not need to be public
 
     [Header("References")]
     public GameObject rockPrefab;
     public GameObject projectilesObject;
     public TextMeshProUGUI throwForceText;
+    public TextMeshProUGUI throwDebounceText;
+    public TextMeshProUGUI rockCountText;
+
+    [Header("Ground Check")]
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+    public float boxXOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -29,38 +43,59 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed;
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+        transform.position += isGrounded() ? new Vector3(Input.GetAxis("Horizontal"), 0, 0) * horizontalSpeed : Vector3.zero;
+        //rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
     }
 
     void Update()
     {
-        throwForceText.text = throwForce.ToString();
-        if (Input.GetMouseButton(0))
+        throwForceText.text = "throwForce: " + Mathf.Clamp(throwForce, 0f, maxThrowForce).ToString("F2");
+        throwDebounceText.text = "throwDebounce: " + throwDebounce.ToString("F3");
+
+        rockCountText.text = "Rocks: " + rockCount.ToString();
+
+        throwDebounce -= throwDebounce <= 0f ? 0f : Time.deltaTime;
+        canThrow = throwDebounce <= 0f;
+
+        if (Input.GetMouseButton(0) && canThrow && rockCount > 0)
         {
             throwing = true;
             throwForce += Time.deltaTime * throwForceMultiplier * 100f;
         }
-        else
+        else if (throwing)
         {
-            if (throwing)
-            {
-                // Find vector/direction to throw player
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 distance = mousePosition - this.transform.position;
-                throwVector = -distance.normalized * throwForce;
-
-                // Create and throw rock + add force to player
-                var newRock = Instantiate(rockPrefab, transform.position, transform.rotation, projectilesObject.transform);
-                newRock.GetComponent<Rigidbody2D>().AddForce(-throwVector);
-                Destroy(newRock, 10);
-
-                rb.AddForce(throwVector, ForceMode2D.Force);
-
-                throwForce = 0f;
-                throwing = false;
-            }
+            throwRock(throwForce <= maxThrowForce / 5f ? initalForce : throwForce);
         }
     }
 
+    private void throwRock(float force)
+    {
+        // Find vector/direction to throw player
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 distance = mousePosition - this.transform.position;
+        throwVector = -distance.normalized * Mathf.Clamp(force, 0f, maxThrowForce);
+
+        // Create and throw rock + add force to player
+        var newRock = Instantiate(rockPrefab, transform.position, transform.rotation, projectilesObject.transform);
+        newRock.GetComponent<Rigidbody2D>().AddForce(-throwVector);
+        Destroy(newRock, 10);
+
+        rb.AddForce(throwVector, ForceMode2D.Force);
+
+        throwForce = 0f;
+        throwing = false;
+        rockCount--;
+        throwDebounce = throwCooldown;
+    }
+
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(transform.position - new Vector3(boxXOffset, 0), boxSize, 0, Vector2.down, castDistance, groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - new Vector3(boxXOffset, 0) - transform.up * castDistance, boxSize);
+    }
 }
